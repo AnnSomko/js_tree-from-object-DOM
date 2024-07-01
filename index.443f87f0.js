532332e14ae740cb4ee08efc0909108bbb3705eb
Anna <ania.somko@gmail.com>
2024-07-01 15:28:56 +0300

    make build

---
+++ index.443f87f0.js
@@ -0,0 +1,2 @@
+!function e(n,t){var r=document.createElement("ul");for(var a in t){var c=document.createElement("li");c.textContent=a,"object"==typeof t[a]&&e(c,t[a]),r.append(c)}r.children.length>0&&n.append(r)}(document.querySelector("#tree"),{Drink:{Wine:{},Schnaps:{}},Fruit:{Red:{Cherry:{},Strawberry:{}},Yellow:{Banana:{},Pineapple:{}}}});
+//# sourceMappingURL=index.443f87f0.js.map
